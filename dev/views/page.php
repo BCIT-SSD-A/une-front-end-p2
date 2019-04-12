ec77920aa27e1e1d302ba3d2b6433700b4dcf3de
une-s <36519297+une-s@users.noreply.github.com>
2019-04-11 21:00:06 -0700

    Minor style changes

--- dev/views/page.php
+++ dev/views/page.php
@@ -5,21 +5,22 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title><?php echo $this->title; ?></title>
+  <link rel="stylesheet" href="css/styles<?php echo ENV_PRODUCTION ? '.min' : '' ?>.css">
 </head>
 <body>
   <div class="page-wrapper">
-    <header>
+    <header class="container">
       <?php
       $header_menu = Menu::get('header');
       echo $header_menu->output(); ?>
     </header>
-    <main>
+    <main class="container">
       <h1 class="page-title">
         <?php echo $this->title; ?>
       </h1>
       <?php $this->output_content(); ?>
     </main>
-    <footer>
+    <footer class="container">
       <p>&copy; 2019 Une Simonsen</p>
     </footer>
   </div>
