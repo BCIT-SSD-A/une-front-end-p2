c4b5e56cd6971e30c221965e8aff2451989f37f8
une-s <36519297+une-s@users.noreply.github.com>
2019-04-17 01:35:55 -0700

    Style home banner, add home text

--- dev/views/pages/home.php
+++ dev/views/pages/home.php
@@ -1,7 +1,15 @@
 <div class="banner">
     <img src="<?php echo Path::IMAGES_URL; ?>/technology-banner.jpg" alt="Technology Banner">
-    <p class="banner-text">
-        Code anything from scratch for any sector. Be an in-demand software programmer.
-    </p>
+    <div class="banner-text">
+        <p>Code anything from scratch for any sector.</p>
+        <p>Be an in-demand software programmer.</p>
+    </div>
 </div>
-<p>Welcome to this website.</p>
+
+<h2>Become a Web Developer Today</h2>
+
+<p>The Software Systems Developer (SSD) program (Web Programmer option) is an eight month, full-time business application development program offered by BCIT's School of Computing and Academic Studies, within the HTP department at the downtown Vancouver campus.</p>
+
+<p>It prepares individuals for high-demand careers in the technical areas of web application development, programming, database and systems analysis. Students learn industry-ready skills through extensive hands-on practice in current scripting and coding techniques, object-oriented architecture patterns, application security and scalability, developer tools, and platforms for building complete web-based business applications from scratch.</p>
+
+<a class="learn-more" href="<?php echo Page::get('overview')->url; ?>">Learn more about SSD</a>
