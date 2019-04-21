95fe8418398c2c2941ede2e7317e5eedfee35aa5
une-s <36519297+une-s@users.noreply.github.com>
2019-04-21 12:27:24 -0700

    Style students page, clickable emails, add heading

--- dev/views/pages/students.php
+++ dev/views/pages/students.php
@@ -1,11 +1,25 @@
 <?php
 $students = Student::get_all();
+$half = ceil(count($students)/2);
+
+$column_1 = array_slice($students, 0, $half);
+$column_2 = array_slice($students, $half);
+
 ?>
-<div class="student-list table">
-  <?php foreach($students as $student) : ?>
-    <div class="student">
-      <div class="full-name"><?php echo $student->full_name; ?></div>
-      <div class="email"><?php echo $student->email; ?></div>
+<h2>Student List</h2>
+<div class="student-list columns">
+  <?php // For each column
+  foreach(array($column_1, $column_2) as $column) : ?>
+    <div class="list-group column">
+      <?php // For each student in column
+      foreach($column as $student) : ?>
+        <div class="student list-group-item">
+          <div class="full-name"><?php echo $student->full_name; ?></div>
+          <div class="email">
+            <a href="mailto:<?php echo $student->email; ?>"><?php echo $student->email; ?></a>
+          </div>
+        </div>
+      <?php endforeach; ?>
     </div>
   <?php endforeach; ?>
 </div>
