19cbd6c97c8de5cc28af19f2b99949640f729fd5
une-s <36519297+une-s@users.noreply.github.com>
2019-04-23 08:58:23 -0700

    Briefly started on staff data

---
+++ dev/data/staff.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+    '1' => array(
+        'name' => 'Pat McGee',
+        'role' => 'SSD Program Lead'
+    ),
+    '2' => array(
+        'name' => 'Phil Weier',
+        'role' => '...'
+    ),
+    '3' => array(
+        'name' => 'Michael Whyte',
+        'role' => 'Instructor'
+    )
+);
